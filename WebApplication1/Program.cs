1538cbc213fc780137159a41d5ad6ae891b972af
Felix Zhang <felix.zhang@xero.com>
2019-03-07 14:31:10 -0500

    remove dep

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,13 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-using Serilog;
 
 [assembly: Fody.ConfigureAwait(false)]
 namespace WebApplication1
@@ -21,7 +13,6 @@ namespace WebApplication1
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-            .UseSerilog((hostingContext, loggerConfiguration) => loggerConfiguration.ReadFrom.Configuration(hostingContext.Configuration));
+                .UseStartup<Startup>();
     }
 }
--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -1,16 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using Serilog;
 
 namespace WebApplication1
 {
@@ -28,9 +20,6 @@ namespace WebApplication1
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-            services.AddLogging(config =>
-                config.AddSerilog(dispose: true)
-            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
