c970b682a81cc19d3fa5f1e9c76e50c46f0d8172
Felix Zhang <felix.zhang@xero.com>
2019-03-07 13:13:51 -0500

    init

---
+++ WebApplication1/Class.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace fsfsfsdfdsf
+{
+    //public static class MethodTimeLogger
+    //{
+    //    static MethodTimeLogger()
+    //    {
+
+    //    }
+
+    //    //todo:use message as tags
+    //    public static void Log(System.Reflection.MethodBase methodBase, long milliseconds, string message)
+    //    {
+    //        Console.Write(milliseconds);
+    //        //if (_metrics == null && _metricsOptions != null)
+    //        //{
+    //        //    _metrics = new DatadogMetrics(_metricsOptions);
+    //        //}
+    //        //if (_metrics != null)
+    //        //{
+    //        //    //_metrics.Timer(Const.profilerNamePrefix + methodBase.DeclaringType.Name + "." + methodBase.Name, milliseconds);
+    //        //}
+    //    }
+
+    //    public static void Log(System.Reflection.MethodBase methodBase, TimeSpan elapsed, string message)
+    //    {
+    //        Console.Write(elapsed.TotalMilliseconds);
+    //        //if (_metrics == null && _metricsOptions != null)
+    //        //{
+    //        //    _metrics = new DatadogMetrics(_metricsOptions);
+    //        //}
+    //        //if (_metrics != null)
+    //        //{
+    //        //    //_metrics.Timer(Const.profilerNamePrefix + methodBase.DeclaringType.Name + "." + methodBase.Name, elapsed.TotalMilliseconds);
+    //        //}
+    //    }
+
+
+    //    public static void shit()
+    //    {
+
+    //    }
+    //}
+}
---
+++ WebApplication1/Controllers/ValuesController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        private readonly ILogger<ValuesController> _logger;
+
+        public ValuesController(ILogger<ValuesController> logger)
+        {
+            _logger = logger;
+        }
+
+        // GET api/values
+        [HttpGet]
+        [MethodTimer.Time]
+        public async Task<ActionResult<IEnumerable<string>>> Get()
+        {
+
+            await new Test().DoWork();
+            return new string[] { "value1", "value2" };
+
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+
+
+    public class Test
+    {
+        [MethodTimer.Time]
+        public async Task<bool> DoWork()
+        {
+            await Task.Delay(500);
+            return true;
+        }
+    }
+}
