7bccf802bf6610f607d4754ce5bd171b00880620
Felix Zhang <felix.zhang@xero.com>
2019-03-07 14:21:03 -0500

    a

---
+++ ClassLibrary2/MethodTimeLogger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace WebApplication1.abc
+{
+    public static class MethodTimeLogger
+    {
+        public static void Log(System.Reflection.MethodBase methodBase, long milliseconds, string message)
+        {
+            Console.Write(milliseconds);
+
+        }
+
+        public static void Log(System.Reflection.MethodBase methodBase, TimeSpan elapsed, string message)
+        {
+            Console.Write(elapsed.TotalMilliseconds);
+        }
+
+    }
+}
